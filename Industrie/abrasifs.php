61d92410a21215cbb8c5a9de6ee2202ec4ff3675
Kevin <Pc@KEVIN.DOMCAPR.LOCAL>
2017-08-09 09:47:58 +0200

    Industrie - Ajout conseil ODIS

--- Industrie/abrasifs.php
+++ Industrie/abrasifs.php
@@ -3,6 +3,7 @@
 
 <div style="margin-top: 120px;">
 	<?php $cheminImg = "../inc/img/Industrie/ABRASIFS/"; ?>
+  <?php $cheminImgBandeau = "../inc/img/Industrie/"; ?>
   <img style="margin-left:5px;float:left" src="<?php echo $cheminImg; ?>Visuel.png" />
   <div style="margin-top: 30px;float:left;margin-left:1%;width:70%">
     <h1 style="color:rgb(0,176,80)"><img src="../inc/img/puce-H1-2.png" />Abrasifs et outils coupants</h1><br />
@@ -30,13 +31,14 @@
     <img style="margin-left: 25px;" src="<?php echo $cheminImg; ?>meules de tronçonnage.png" />
     <img style="margin-left: 25px;" src="<?php echo $cheminImg; ?>disques de tronçonnage.png" />
   </div>
-  <div style="margin-top: 30px;float:left;margin-left:1%;width:70%;">
-    <p style="margin-left:95px;font-size:1.3em;border:2px solid rgb(0,204,102);">Dans le choix de votre papier abrasif, les étapes de ponçage vont déterminer le grain de votre abrasif : moins de 50 grains : enlèvement de matière de 50 à 80 grains : décapage, dégrossissage, première passe de ponçage de 80 à 120 grains : ponçage, 120 et plus : Finition plus ou moins poussée.
+  <div style="margin-top: 100px;float:left;margin-left:1%;width:70%;">
+    <p style="margin-left:95px;font-size:1.3em;border:2px solid rgb(0,204,102);padding:15px">
+    <img src="<?php echo $cheminImgBandeau; ?>conseilOdis.PNG" style="margin-left:10px;margin-top:-90px;"/><br />
+    Dans le choix de votre papier abrasif, les étapes de ponçage vont déterminer le grain de votre abrasif : moins de 50 grains : enlèvement de matière de 50 à 80 grains : décapage, dégrossissage, première passe de ponçage de 80 à 120 grains : ponçage, 120 et plus : Finition plus ou moins poussée.
     </p>
   </div>  
 </div><!-- /.container -->
 
-  <?php $cheminImgBandeau = "../inc/img/Industrie/"; ?>
   <div style="display:inline;float:left;margin-left:30px;width:1000px;font-size:1.1em;">
     <marquee behavior="alternate" scrollamount="2" scrolldelay="50" height="180" width="100%" style="margin-top:50px;text-align: center;" direction="right">
       <img alt="Bosal" src="<?php echo $cheminImgBandeau; ?>/Logos bandeau/Brain Bee.png"  border="0">
